1a6365c1cbc2720c8db6ca70a49cd1be3ed59ec5
XTLiuuu <liuxuantong0611@gmail.com>
2018-06-27 13:32:19 -0400

    change port number

--- config/auth.js
+++ config/auth.js
@@ -2,9 +2,9 @@ module.exports = {
 
 
     'googleAuth' : {
-        'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
-        'clientSecret'  : 'iqgLqu6pXgLuHsZFq6nvxDX3',
-        'callbackURL'   : 'http://127.0.0.1:5000/login/authorized'
+        'clientID': '264994039615-1anlorifkgrlttvo5mdstua6ppvvbvda.apps.googleusercontent.com',
+        'clientSecret'  : '4ZZbi9__xmIh9ZVRujiM6Y3Z',
+        'callbackURL'   : 'http://127.0.0.1:6500/login/authorized'
     }
 
 };
