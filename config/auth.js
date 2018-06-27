27253fd46263821a3e82f7d5eefd19ffe37bb09a
XTLiuuu <liuxuantong0611@gmail.com>
2018-06-27 14:16:24 -0400

    how

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'googleAuth' : {
         'clientID': '264994039615-1anlorifkgrlttvo5mdstua6ppvvbvda.apps.googleusercontent.com',
         'clientSecret'  : '4ZZbi9__xmIh9ZVRujiM6Y3Z',
-        'callbackURL'   : 'http://leiner.cs-i.brandeis.eduhttp://leiner.cs-i.brandeis.edu:6500/login/authorized'
+        'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:6500/login/authorized'
     }
 
 };
