645c46bbacd544957cc806bcf4db463cde2249a5
XTLiuuu <liuxuantong0611@gmail.com>
2018-06-29 13:12:21 -0400

    change URL

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,8 @@ module.exports = {
     'googleAuth' : {
         'clientID': '264994039615-1anlorifkgrlttvo5mdstua6ppvvbvda.apps.googleusercontent.com',
         'clientSecret'  : '4ZZbi9__xmIh9ZVRujiM6Y3Z',
-        'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:6500/login/authorized'
+        'callbackURL'   : 'http://127.0.0.1:6500/login/authorized'
+        //'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:6500/login/authorized'
     }
 
 };
